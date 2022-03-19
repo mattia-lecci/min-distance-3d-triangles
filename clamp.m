c6d5a10c528a868edfa2cf283c63942afd71d39d
mattia-lecci <mattia.lecci@gmail.com>
2022-03-19 16:59:52 +0100

    Add documentation to clamp function

--- clamp.m
+++ clamp.m
@@ -1,2 +1,13 @@
-function y = clamp(x,lb,ub)
-  y=min( max(x, lb), ub );
+function y = clamp(x, lb, ub)
+%CLAMP Clamp a value between a lower and an upper bound
+%
+% INPUT:
+% - x: input value
+% - lb: lower bound
+% - ub: upper bound
+%
+% OUTPUT:
+% - y: clamped value
+
+assert(lb <= ub, 'Lower bound must not be greater than upper bound');
+y = min(max(lb, x), ub);
